588c58cf15c0107e8de1bfd3fe3fbcccaed248f3
Abdaaa1 <abdallaheljai@gmail.com>
2025-01-09 20:45:13 -0500

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "MTMyNzA2ODc2NzEyNTExNTAzMw.GRlYrg.TLNl8EOz75VBjo8fVnNmTcfZujq1y-0vEawrB8",
+    prefix: "s!",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "PEOPLE WHO CAN USE ADD MONEY (1006526865982378015)"
 ],
     debug: true,
-    countChannel: "countChannelID"
+    countChannel: "1327090583722004520"
 };
